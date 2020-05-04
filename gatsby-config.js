f1bc32011259f9d180678acaa31e926c9cdd43d4
deeon-roy <55064506+deeon-roy@users.noreply.github.com>
2020-05-04 23:29:51 +0100

    Update gatsby-config.js
    change name

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description:
       'Retro is a modern stylish one page resume template built using React and Gatsby',
   },
-  pathPrefix: '/portfolio_',
+  pathPrefix: '/portfolio',
   plugins: [
     {
       resolve: 'gatsby-source-filesystem',
